69e4d739dd8cf66702997a84c0af91d99b155163
crazyfundu <anvesh.shamra@gmail.com>
2014-12-20 03:17:29 +0530

    iminmaxstack

---
+++ AlgorithmSoloutions/src/com/crazyfundu/github/algorithmsolutions/stack/IMinMaxStack.java
@@ -0,0 +1,15 @@
+package com.crazyfundu.github.algorithmsolutions.stack;
+
+public interface IMinMaxStack extends IStack {
+	/*
+	 * Will return the minimum value from the stack
+	 */
+	public int min();
+	
+	/*
+	 * Will return the maxumum value from the stack
+	 */
+	public int max();
+	
+	
+}
