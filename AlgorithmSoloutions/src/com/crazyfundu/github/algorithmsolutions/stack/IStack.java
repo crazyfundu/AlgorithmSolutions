495f494aaf8563e8c28dec7a7981d0d0fb54498c
crazyfundu <anvesh.shamra@gmail.com>
2014-12-20 03:00:31 +0530

    Adding Stack

---
+++ AlgorithmSoloutions/src/com/crazyfundu/github/algorithmsolutions/stack/IStack.java
@@ -0,0 +1,29 @@
+package com.crazyfundu.github.algorithmsolutions.stack;
+/**
+ * 
+ * @author AvneeshS1
+ *
+ */
+public interface IStack<E> {
+	/*
+	 * Placing the data on top of the stack
+	 */
+	public void push(E element );
+	/*
+	 * Returning the data and removing it from the top
+	 */
+	public E pop();
+	/*
+	 *Reading the value from the top
+	 */
+	public E peep();
+	/*
+	 * Return the current size of the stack
+	 */
+	public int getSize();
+	
+	/*
+	 * Check whether the Stack is empty
+	 */
+	public boolean isEmpty();
+}
