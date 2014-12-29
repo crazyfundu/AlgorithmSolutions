68fabac15ca3a83d5c196fe521f36a0a9b2f1a37
crazyfundu <anvesh.shamra@gmail.com>
2014-12-29 19:46:11 +0530

    quick sort one more implementation

--- AlgorithmSoloutions/src/com/crazyfundu/github/algorithmsolutions/sortingandsearching/GenericImplementations/QuickSort.java
+++ AlgorithmSoloutions/src/com/crazyfundu/github/algorithmsolutions/sortingandsearching/GenericImplementations/QuickSort.java
@@ -13,7 +13,8 @@ public class QuickSort<E extends Comparable<E>> {
 		list.add(7);
 		list.add(1);
 		QuickSort<Integer> bs = new QuickSort<Integer>();
-		System.out.println(bs.sort(list));
+		//System.out.println(bs.sort(list));
+		bs.sort2(list);
 	}
 	
 	//Complex and implemented by me.
@@ -68,4 +69,56 @@ public class QuickSort<E extends Comparable<E>> {
 		System.out.println(l);
 		return l; 
 	}
+	
+	public void sort2(List<E> list){
+		int size = list.size();
+		int pivotIndex = size/2;
+		
+		E pivot = list.get(pivotIndex);
+		
+		int i = 0;
+		int j = list.size()-1;
+		
+		while(i<j ){
+			
+			while(list.get(i).compareTo(pivot) == -1 ){
+				i++;
+			}
+			while(list.get(j).compareTo(pivot) ==  1){
+				j--;
+			}
+			
+			if(i<j ){
+				E temp = list.get(i);
+				list.set(i, list.get(j));
+				list.set(j, temp);
+				j--;i++;
+			}
+		}
+
+		System.out.println(list);
+
+		if(0 < j )
+			sort2(list.subList(0, j));
+		if(i < size-1)
+			sort2(list.subList(i, size));
+		
+		System.out.println(list);
+		
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
